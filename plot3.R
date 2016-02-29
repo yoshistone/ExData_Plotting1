c976e8ff3ed44f4dd81fe0db82f4c81aa8efc2a0
yoshistone <amelia.o.stone@gmail.com>
2016-02-29 00:07:26 -0800

    added the third and fourth plots

---
+++ plot3.R
@@ -0,0 +1,15 @@
+data <- read.csv("household_power_consumption.txt", sep = ";")
+datetime <- strptime(paste(data$Date, data$Time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
+newdata <- cbind(data, datetime)
+newdata$Date <- as.Date(newdata$Date, format = "%d/%m/%Y")
+newdata$Sub_metering_1 <- as.numeric(as.character(newdata$Sub_metering_1))
+newdata$Sub_metering_2 <- as.numeric(as.character(newdata$Sub_metering_2))
+newdata$Sub_metering_3 <- as.numeric(as.character(newdata$Sub_metering_3))
+Newdata <- newdata[newdata$Date >= "2007-02-01" & newdata$Date <= "2007-02-02",]
+with(Newdata, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
+lines(Newdata$datetime, Newdata$Sub_metering_2, type = "l", col = "red")
+lines(Newdata$datetime, Newdata$Sub_metering_3, col = "blue")
+legend("topright", lty=1, col = c("black", "red", "blue"), cex = 0.7, legend = c("Sub_metering_1", "Sub_metering_2",
+  "Sub_metering_3"))                                                                    
+dev.copy(png, file="plot3.png", width = 480, height = 480)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,11 @@
+par(mfrow = c(2,2))
+with(Newdata, plot(datetime, Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)"))
+with(Newdata, plot(datetime, Voltage, type = "l", ylab = "Voltage"))
+with(Newdata, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
+lines(Newdata$datetime, Newdata$Sub_metering_2, type = "l", col = "red")
+lines(Newdata$datetime, Newdata$Sub_metering_3, col = "blue")
+legend("topright", lty=1, col = c("black", "red", "blue"), cex = 0.7, legend = c("Sub_metering_1", "Sub_metering_2",
+                                                                                 "Sub_metering_3"))    
+with(Newdata, plot(datetime, Global_reactive_power, type = "l", ylab = "Global_reactive_power"))
+dev.copy(png, file="plot4.png", width = 480, height = 480)
+dev.off()
